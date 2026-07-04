c29561cffcde04a05c431dfaa7dc518454c0b970
Brii <141.brii@gmail.com>
2026-07-04 16:50:06 -0500

    Week 1 Assignment

--- assignment1/assignment1.py
+++ assignment1/assignment1.py
@@ -1,1 +1,7 @@
-# Write your code here.
+#Write your code here.
+def say_hello ():
+    print("Hello Sunshine!")
+def greet(name):
+    return "Hello, " + name + "!"
+greet("Bria")
+print (greet("Bria"))
