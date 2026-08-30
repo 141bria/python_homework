331a66969812cbdcf6c5b0ba55e59ddc6dbf8cc9
Brii <141.brii@gmail.com>
2026-08-30 18:47:11 -0500

    week 9 tweaks 08.30

--- assignment9/sql_intro.py
+++ assignment9/sql_intro.py
@@ -2,32 +2,31 @@ import sqlite3
 try:
     with sqlite3.connect("../db/magazines.db") as conn:
         cursor = conn.cursor()
+        cursor.execute("""
+        CREATE TABLE IF NOT EXISTS Publishers(
+        publisher_name TEXT NOT NULL UNIQUE,
+        publisher_id INTEGER PRIMARY KEY
+        ) 
+        """)
+        cursor.execute("""
+        CREATE TABLE IF NOT EXISTS Magazines(
+        magazine_name TEXT NOT NULL UNIQUE,
+        magazine_id INTEGER PRIMARY KEY,
+        publisher_id INTERGER
+        ) 
+        """)
+        cursor.execute("""
+        CREATE TABLE IF NOT EXISTS Subscibers(
+        subscriber_name TEXT NOT NULL UNIQUE,
+        subscriber_address TEXT NOT NULL UNIQUE,
+        subscriber_id INTEGER,
+        magazine_id INTEGER
+            ) 
+            """)
+        cursor.execute("""
+        CREATE TABLE IF NOT EXISTS Subscriptions(
+        expirate_date TEXT NOT NULL
+        ) 
+    """)
 except sqlite3.Error as e:
-        print(e)
-        cursor.execute("""""
-    CREATE TABLE IF NOT EXSISTS Publishers(
-publisher_name TEXT NOT NULL UNIQUE,
-publisher_id INTEGER PRIMARY KEY
-) 
-""")
-        cursor.execute("""""
-        CREATE TABLE IF NOT EXSISTS Magazines(
-magazine_name TEXT NOT NULL UNIQUE,
-magazine_id INTEGER PRIMARY KEY,
-publisher_id INTERGER
-) 
-""")
-        cursor.execute("""""
-        CREATE TABLE IF NOT EXSISTS Subscibers(
-subscriber_name TEXT NOT NULL UNIQUE,
-subscriber_address TEXT NOT NULL UNIQUE,
-subscriber_id INTEGER,
-magazine_id INTEGER,
-e
-) 
-""")
-        cursor.execute("""""
-        CREATE TABLE IF NOT EXSISTS Subscriptions(
-expirate_date TEXT NOT NULL
-) 
-""")
+            print(e)
