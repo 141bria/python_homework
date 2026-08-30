d4fea0ad1865ea018b3460d114fbab321d908dc5
Brii <141.brii@gmail.com>
2026-08-30 16:28:38 -0500

    assignment9

--- assignment9/sql_intro.py
+++ assignment9/sql_intro.py
@@ -6,17 +6,28 @@ except sqlite3.Error as e:
     print(e)
 cursor.execute("""""
 CREATE TABLE IF NOT EXSISTS Publishers(
+publisher_name TEXT NOT NULL UNIQUE,
+publisher_id INTEGER PRIMARY KEY
 ) 
 """)
 cursor.execute("""""
 CREATE TABLE IF NOT EXSISTS Magazines(
+magazine_name TEXT NOT NULL UNIQUE,
+magazine_id INTEGER PRIMARY KEY,
+publisher_id INTERGER
 ) 
 """)
 cursor.execute("""""
 CREATE TABLE IF NOT EXSISTS Subscibers(
+subscriber_name TEXT NOT NULL UNIQUE,
+subscriber_address TEXT NOT NULL UNIQUE,
+subscriber_id INTEGER,
+magazine_id INTEGER,
+e
 ) 
 """)
 cursor.execute("""""
 CREATE TABLE IF NOT EXSISTS Subscriptions(
+expirate_date TEXT NOT NULL
 ) 
 """)
