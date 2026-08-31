d3283605e25573d7cd86883463acdeecd453d491
Brii <141.brii@gmail.com>
2026-08-30 19:21:41 -0500

    Update Week 9 SQL homework

--- assignment9/sql_intro.py
+++ assignment9/sql_intro.py
@@ -1,6 +1,7 @@
 import sqlite3
 try:
     with sqlite3.connect("../db/magazines.db") as conn:
+        conn.execute("PRAGMA foreign_keys = 1")
         cursor = conn.cursor()
         cursor.execute("""
         CREATE TABLE IF NOT EXISTS Publishers(
@@ -30,3 +31,20 @@ try:
     """)
 except sqlite3.Error as e:
             print(e)
+def add_publisher(cursor,publisher_name,publisher_id):
+    try:
+        cursor.execute("INSERT INTO Publishers (publisher_name,publisher_id)) VALUES (?,?)",(publisher_name, publisher_id))
+    except sqlite3.IntegrityError:
+          print(f"{publisher_name} is already in the datatbase.")
+
+def add_magazines(cursor,magazine_name,magazine_id,publisher_id):
+    try:
+        cursor.execute("INSERT INTO Magazines (magazine_name,magazine_id,publisher_id) VALUES (?,?,?)",(magazine_name,magazine_id,publisher_id))
+    except sqlite3.IntegrityError:
+          print(f"{magazine_name} is already in the datatbase.")
+
+def add_subscriber(cursor,subscriber_name,subscriber_address,subscriber_id,magazine_id):
+    try:
+        cursor.execute("INSERT INTO Subscribers (subscriber_name,subscriber_address,subscriber_id,magazine_id) VALUES (?,?,?,?)",(subscriber_name,subscriber_address,subscriber_id,magazine_id))
+    except sqlite3.IntegrityError:
+          print(f"{subscriber_name} is already in the datatbase.")
